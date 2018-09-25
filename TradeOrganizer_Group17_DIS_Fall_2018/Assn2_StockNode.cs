eaca055af4ac7f70306caf42435c9bab8d2b688f
nipunsoni <nipun.soni@ymail.com>
2018-09-24 20:20:50 -0400

    Initial commit

---
+++ TradeOrganizer_Group17_DIS_Fall_2018/Assn2_StockNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradeOrganizer_Group17_DIS_Fall_2018
+{
+  public class StockNode
+  {
+    public Stock StockHolding;
+    public StockNode Next;
+
+    // default constructor
+    public StockNode()
+    {
+      StockHolding = null;
+      Next = null;
+    }
+
+    //constructor to initialize the variables of object
+    public StockNode(Stock stockHolding)
+    {
+      StockHolding = stockHolding;
+      Next = null;
+    }
+  }
+}
