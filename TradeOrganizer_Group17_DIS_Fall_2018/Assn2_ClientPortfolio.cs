a7e3361c7f9367ccf930ce6735acc63e0306c914
nipunsoni <nipun.soni@ymail.com>
2018-09-30 19:54:31 -0400

    Updated files from Prof -Client Profile and Program

--- TradeOrganizer_Group17_DIS_Fall_2018/Assn2_ClientPortfolio.cs
+++ TradeOrganizer_Group17_DIS_Fall_2018/Assn2_ClientPortfolio.cs
@@ -1,35 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TradeOrganizer_Group17_DIS_Fall_2018
+﻿namespace TradeOrganizer_Group17_DIS_Fall_2018
 {
-  public class ClientPortfolio
-  {
-    public StockList StockList { get; set; }
-
-    public decimal PortfolioValue { get; set; }
-    public string HolderName { get; set; }
-    public string HolderAddress { get; set; }
-    public string ContactNumber { get; set; }
-
-    // default constructor
-    public ClientPortfolio()
+    public class ClientPortfolio
     {
-      StockList = new StockList();
-      HolderName = null;
-      HolderAddress = null;
-      ContactNumber = null;
-    }
+        public StockList StockList { get; set; }
 
-    // Construtor for initialization
-    public ClientPortfolio(string holderName, string holderAddress, string contactNumber)
-    {
-      StockList = new StockList();
-      PortfolioValue = 0.0m;
-      HolderName = holderName;
-      HolderAddress = holderAddress;
-      ContactNumber = contactNumber;
+        public decimal PortfolioValue { get; set; }
+        public string HolderName { get; set; }
+        public string HolderAddress { get; set; }
+        public string ContactNumber { get; set; }
+
+        // default constructor
+        public ClientPortfolio()
+        {
+            StockList = new StockList();
+            HolderName = null;
+            HolderAddress = null;
+            ContactNumber = null;
+        }
+
+        // Construtor for initialization
+        public ClientPortfolio(string holderName, string holderAddress, string contactNumber)
+        {
+            StockList = new StockList();
+            PortfolioValue = 0.0m;
+            HolderName = holderName;
+            HolderAddress = holderAddress;
+            ContactNumber = contactNumber;
+        }
+
+        public decimal GetPortfolioValue()
+        {
+            return this.StockList.Value();
+        }
     }
-  }
 }
