b74f289fd2290cc1d0285479185d5e6dd81acc00
nipunsoni <nipun.soni@ymail.com>
2018-09-30 21:10:27 -0400

    TradeOrganizer commit - StockList Student 3 file - Final changes

--- TradeOrganizer_Group17_DIS_Fall_2018/Assn2_StockList_Students_3.cs
+++ TradeOrganizer_Group17_DIS_Fall_2018/Assn2_StockList_Students_3.cs
@@ -1,45 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TradeOrganizer_Group17_DIS_Fall_2018
 {
-  public partial class StockList
-  {
-    //param        : NA
-    //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
-    //return       : total value
-    //return type  : decimal
-    public decimal Value()
+    public partial class StockList
     {
-      decimal value = 0.0m;
+        //param        : NA
+        //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
+        //return       : total value
+        //return type  : decimal
+        public decimal Value()
+        {
+            decimal value = 0.0m;
 
-      // write your implementation here
+            //assign head of the list to StockNode object
+            StockNode current = head;
 
-      return value;
+            //Traverse Linked list till we reach end
+            while (current != null)
+            {
+                //keep adding value of each node by multiplying holdings with price, and returns the total value
+                value = value + (current.StockHolding.Holdings * current.StockHolding.CurrentPrice);
+
+                //set current to be Next node
+                current = current.Next;
+            }
+
+            return value;
+        }
+
+        //param  (StockList) listToCompare     : StockList which has to comared for similarity index
+        //summary      : finds the similar number of nodes between two lists
+        //return       : similarty index
+        //return type  : int
+        public int Similarity(StockList listToCompare)
+        {
+            int similarityIndex = 0;
+
+            //assign head of the listToCompare to StockNode object
+            StockNode current = listToCompare.head;
+
+            //Traverse first list till we reach end
+            while (head != null)
+            {
+
+                //Traverse listToCompare list till we reach end
+                while (current != null)
+                {
+                    //Check for similarity in name and increment similarityIndex
+                    if (current.StockHolding.Name.Equals(head.StockHolding.Name))
+                    {
+                        similarityIndex++;
+                        break;
+                    }
+
+                    //set current to be Next node
+                    current = current.Next;
+                }
+
+                //move heads of both the linked lists
+                head = head.Next;
+                current = listToCompare.head;
+            }
+
+            return similarityIndex;
+        }
+
+        //param        : NA
+        //summary      : Print all the nodes present in the list
+        //return       : NA
+        //return type  : NA
+        public void Print()
+        {
+            //assign head of the list to StockNode object
+            StockNode current = head;
+
+            //Traverse Linked list till we reach end
+            while (current != null)
+            {
+                //Print list to console
+                Console.WriteLine(current.StockHolding);
+
+                //set current to be Next node
+                current = current.Next;
+            }
+
+        }
     }
-
-    //param  (StockList) listToCompare     : StockList which has to comared for similarity index
-    //summary      : finds the similar number of nodes between two lists
-    //return       : similarty index
-    //return type  : int
-    public int Similarity(StockList listToCompare)
-    {
-      int similarityIndex = 0;
-
-      // write your implementation here
-
-      return similarityIndex;
-    }
-
-    //param        : NA
-    //summary      : Print all the nodes present in the list
-    //return       : NA
-    //return type  : NA
-    public void Print()
-    {
-      // write your implementation here
-
-    }
-  }
 }
